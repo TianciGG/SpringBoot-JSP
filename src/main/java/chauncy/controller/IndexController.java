03bec94dd5405692a44d5a803019523a5b8e704c
ChauncyWang <chauncy_wang@163.com>
2019-05-14 17:31:22 +0800

    cache at 2019-05-14 17:31

---
+++ src/main/java/chauncy/controller/IndexController.java
@@ -0,0 +1,21 @@
+package chauncy.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/IndexController")
+@EnableAutoConfiguration
+public class IndexController {
+	
+	@RequestMapping("/index")
+	public String index(){
+		return "index";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(IndexController.class, args);
+	}
+}
